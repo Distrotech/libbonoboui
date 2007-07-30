87ea2c43dec919b10f4d9a9d49624bbc46c0c9ba
Christian Persch <chpe@gnome.org>
2007-07-30 15:34:37 +0000

    ============================= 2.19.6 =====================
    2007-07-30  Christian Persch  <chpe@gnome.org>
    
    	* autogen.sh:
    	* configure.in: Build fixes.
    
    svn path=/trunk/; revision=2526

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="libbonoboui"
-REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/autogen.sh \
@@ -15,30 +14,4 @@ REQUIRED_AUTOMAKE_VERSION=1.9
     exit 1
 }
 
-DIE=0
-
-# This is a bit complicated here since we can't use gnome-config yet.
-# It'll be easier after switching to pkg-config since we can then
-# use pkg-config to find the gnome-autogen.sh script.
-
-gnome_autogen=
-gnome_datadir=
-
-ifs_save="$IFS"; IFS=":"
-for dir in $PATH ; do
-  test -z "$dir" && dir=.
-  if test -f $dir/gnome-autogen.sh ; then
-    gnome_autogen="$dir/gnome-autogen.sh"
-    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
-    break
-  fi
-done
-IFS="$ifs_save"
-
-if test -z "$gnome_autogen" ; then
-  echo "You need to install the gnome-common module and make"
-  echo "sure the gnome-autogen.sh script is in your \$PATH."
-  exit 1
-fi
-
-GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 . $gnome_autogen
+REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
